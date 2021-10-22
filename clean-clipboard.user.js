3e959f99a1135cee66007b6f4b754a880693b7ea
Miro11 <69784329+Miroxyz@users.noreply.github.com>
2021-10-22 08:44:29 +0800

    Update clean-clipboard.user.js

--- clean-clipboard.user.js
+++ clean-clipboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         净化剪贴板
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  从 xxx 网站复制的文字都会被加上版权声明，这个脚本会去除这个版权声明
 // @author       Miroxyz
 // @match        *://*.bilibili.com/*
@@ -17,6 +17,13 @@
   document.addEventListener("copy", function (e) {
     e.preventDefault();
     var copytext = window.getSelection().toString();
-    navigator.clipboard.writeText(copytext);
+    navigator.clipboard.writeText(copytext).then(
+      function () {
+        /* clipboard successfully set */
+      },
+      function () {
+        /* clipboard write failed */
+      }
+    );
   });
 })();
